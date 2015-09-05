23bf9e15fda083a46d7553f62b5156108bd94296
Ron van Kesteren <mail@ronvankesteren.nl>
2015-09-05 16:05:59 +0200

    Geen onbekende gegevens laten zien in competitiedatabase

--- angularapps/competitieapp/js/controllers.js
+++ angularapps/competitieapp/js/controllers.js
@@ -60,84 +60,128 @@ competitieControllers.controller('CompetitiesCtrl', ['$scope', '$http', '$locati
                          'Indoor/Outdoor');  
 }]);
 
-competitieControllers.controller('UitslagenCtrl', ['$scope', '$routeParams', '$http', '$location', 'Klassementen', 'Wedstrijden', 'Opzet',
-  function ($scope, $routeParams, $http, $location, Klassementen, Wedstrijden, Opzet) {
-      $scope.competitie = $routeParams.competitie;
-      $scope.divisies = [];
-      $scope.speeldagen = [];
-      $scope.selectieSpeeldag = '';
-      $scope.selectieDivisie = '';
+competitieControllers.controller('UitslagenCtrl', ['$scope', '$routeParams', '$http', '$location', '$filter', 'Klassementen', 'Wedstrijden', 'Opzet',
+    function ($scope, $routeParams, $http, $location, $filter, Klassementen, Wedstrijden, Opzet) {
+        $scope.competitie = $routeParams.competitie;
+        $scope.divisies = [];
+        $scope.speeldagen = [];
+        $scope.selectieSpeeldag = '';
+        $scope.selectieDivisie = '';
 
-      $scope.updateSpeeldagen = function (selectedDivisie) {
-          if (!$scope.opzet.some(function (value) { return (value.divisie == selectedDivisie && value.speeldag == $scope.selectieSpeeldag); })) {
-              $scope.opzet.some(function (value) {
-                  if (value.divisie == selectedDivisie) {
-                      $scope.selectSpeeldag(value.speeldag);
-                      return true;
-                  }
-                  return false;
-              });
-          }
-      }
+        $scope.showSpiritRanking = true;
+        $scope.showScorePunten = true;
+        $scope.showScoreWedstrijden = true;
+        $scope.showScoreVoor = true;
+        $scope.showScoreTegen = true;
+        $scope.showScoreSpirit = true;
 
-      $scope.updateDivisies = function (selectedSpeeldag) {
-          if (!$scope.opzet.some(function (value) { return (value.speeldag == selectedSpeeldag && value.divisie == $scope.selectieDivisie); })) {
-              $scope.opzet.some(function (value) {
-                  if (value.speeldag == selectedSpeeldag) {
-                      $scope.selectDivisie(value.divisie);
-                      return true;
-                  }
-                  return false;
-              });
-          }
-      }
+        $scope.filterColumn = function (field) {
+            var sum = 0;
+            for (var value in $scope.filteredKlassementen) {
+                regel = $scope.filteredKlassementen[value];
+                sum += regel[field];
+            }
+            return sum > 0;
+        };
 
-      $scope.selectDivisie = function (divisie) {
-          $scope.selectieDivisie = divisie;
-          $scope.dropdownMenu = '';
-          $scope.divisieDropdownText = divisie == '' ? 'Alle divisies' : 'Divisie ' + divisie;
+        $scope.filterColumns = function () {
+            $scope.showSpiritRanking = $scope.filterColumn('ranking_spirit');
+            $scope.showScorePunten = $scope.filterColumn('score_punten');
+            $scope.showScoreWedstrijden = $scope.filterColumn('score_wedstrijden');
+            $scope.showScoreVoor = $scope.filterColumn('score_voor');
+            $scope.showScoreTegen = $scope.filterColumn('score_tegen');
+            $scope.showScoreSpirit = $scope.filterColumn('score_spirit');
+        };
 
-          search = $location.search();
-          search.divisie = divisie;
-          $location.search(search).replace();
-      };
+        $scope.$watch('selectieSpeeldag', function (newvalue) {
+            $scope.filteredKlassementen = $filter('divisie')($filter('speeldag')($scope.klassementen, $scope.selectieSpeeldag), $scope.selectieDivisie);
+            $scope.filteredWedstrijden = $filter('divisie')($filter('speeldag')($scope.wedstrijden, $scope.selectieSpeeldag), $scope.selectieDivisie);
+            $scope.filterColumns();
+        });
 
-      $scope.selectSpeeldag = function (speeldag) {
-          $scope.selectieSpeeldag = speeldag;
-          $scope.dropdownMenu = '';
-          $scope.speeldagDropdownText = speeldag == '' ? 'Eindstand' : 'Speeldag ' + speeldag;
-
-          search = $location.search();
-          search.speeldag = speeldag;
-          $location.search(search).replace();
-      };
-
-      $scope.naarCompetities = function () {
-          
-         
-          $location.path('competities/');        
-      };
-
-      $scope.klassementen = Klassementen.query({ competitie: $routeParams.competitie });
-      $scope.wedstrijden = Wedstrijden.query({ competitie: $routeParams.competitie });
-
-      $scope.opzet = Opzet.query({ competitie: $routeParams.competitie }, function (opzet) {
-          angular.forEach(opzet, function (value, key) {
-              if ($.inArray(value.divisie, $scope.divisies) == -1) {
-                  $scope.divisies.push(value.divisie);
-              }
-
-              if ($.inArray(value.speeldag, $scope.speeldagen) == -1) {
-                  $scope.speeldagen.push(value.speeldag);
-              }
-          });
+        $scope.$watch('selectieDivisie', function (newvalue) {
+            $scope.filteredKlassementen = $filter('divisie')($filter('speeldag')($scope.klassementen, $scope.selectieSpeeldag), $scope.selectieDivisie);
+            $scope.filteredWedstrijden = $filter('divisie')($filter('speeldag')($scope.wedstrijden, $scope.selectieSpeeldag), $scope.selectieDivisie);
+            $scope.filterColumns();
+        });
 
 
-          search = $location.search();
-          $scope.selectieType = (search.type == 'K') ? 'K' : 'C';
-          $scope.selectSpeeldag($scope.speeldagen.indexOf(search.speeldag) >= 0 ? search.speeldag : $scope.speeldagen[0]);
-          $scope.selectDivisie($scope.divisies.indexOf(search.divisie) >= 0 ? search.divisie : $scope.divisies[0]);
-          $scope.updateSpeeldagen($scope.selectieDivisie);
-      });
-  } ]);
+        $scope.updateSpeeldagen = function (selectedDivisie) {
+            if (!$scope.opzet.some(function (value) { return (value.divisie == selectedDivisie && value.speeldag == $scope.selectieSpeeldag); })) {
+                $scope.opzet.some(function (value) {
+                    if (value.divisie == selectedDivisie) {
+                        $scope.selectSpeeldag(value.speeldag);
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+
+        $scope.updateDivisies = function (selectedSpeeldag) {
+            if (!$scope.opzet.some(function (value) { return (value.speeldag == selectedSpeeldag && value.divisie == $scope.selectieDivisie); })) {
+                $scope.opzet.some(function (value) {
+                    if (value.speeldag == selectedSpeeldag) {
+                        $scope.selectDivisie(value.divisie);
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+
+        $scope.selectDivisie = function (divisie) {
+            $scope.selectieDivisie = divisie;
+            $scope.dropdownMenu = '';
+            $scope.divisieDropdownText = divisie == '' ? 'Alle divisies' : 'Divisie ' + divisie;
+
+            search = $location.search();
+            search.divisie = divisie;
+            $location.search(search).replace();
+        };
+
+        $scope.selectSpeeldag = function (speeldag) {
+            $scope.selectieSpeeldag = speeldag;
+            $scope.dropdownMenu = '';
+            $scope.speeldagDropdownText = speeldag == '' ? 'Eindstand' : 'Speeldag ' + speeldag;
+
+            search = $location.search();
+            search.speeldag = speeldag;
+            $location.search(search).replace();
+        };
+
+        $scope.naarCompetities = function () {
+
+
+            $location.path('competities/');
+        };
+
+        $scope.klassementen = Klassementen.query({ competitie: $routeParams.competitie }, function (klassementen) {
+            $scope.filteredKlassementen = $filter('divisie')($filter('speeldag')(klassementen, $scope.selectieSpeeldag), $scope.selectieDivisie);
+            $scope.filterColumns();
+        });
+
+        $scope.wedstrijden = Wedstrijden.query({ competitie: $routeParams.competitie }, function (wedstrijden) {
+            $scope.filteredWedstrijden = $filter('divisie')($filter('speeldag')(wedstrijden, $scope.selectieSpeeldag), $scope.selectieDivisie);
+        });
+
+        $scope.opzet = Opzet.query({ competitie: $routeParams.competitie }, function (opzet) {
+            angular.forEach(opzet, function (value, key) {
+                if ($.inArray(value.divisie, $scope.divisies) == -1) {
+                    $scope.divisies.push(value.divisie);
+                }
+
+                if ($.inArray(value.speeldag, $scope.speeldagen) == -1) {
+                    $scope.speeldagen.push(value.speeldag);
+                }
+            });
+
+
+            search = $location.search();
+            $scope.selectieType = (search.type == 'K') ? 'K' : 'C';
+            $scope.selectSpeeldag($scope.speeldagen.indexOf(search.speeldag) >= 0 ? search.speeldag : $scope.speeldagen[0]);
+            $scope.selectDivisie($scope.divisies.indexOf(search.divisie) >= 0 ? search.divisie : $scope.divisies[0]);
+            $scope.updateSpeeldagen($scope.selectieDivisie);
+        });
+    } ]);
   
